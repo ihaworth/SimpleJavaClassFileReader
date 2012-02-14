7f589c424606aec4b78c42c323ccf84b4f017f01
U-Ian-HP\Ian <Ian@Ian-HP.(none)>
2012-02-14 20:58:25 +0000

    Read the first few items from the classfile

---
+++ src/JavaClassFileReader.java
@@ -0,0 +1,74 @@
+import java.io.IOException;
+import java.io.InputStream;
+
+
+public class JavaClassFileReader
+{
+    private static final int MAGIC_NUMBER_LENGTH = 4;
+
+    private final InputStream inputStream;
+
+    private byte[] magicNumbers;
+    private int minorVersion;
+    private int majorVersion;
+
+    private Object constantPoolSize;
+
+    public JavaClassFileReader(InputStream inputStream) throws IOException
+    {
+        this.inputStream = inputStream;
+        readMagicNumber();
+        readMinorVersion();
+        readMajorVersion();
+        readConstantPoolSize();
+    }
+
+    private void readMagicNumber() throws IOException
+    {
+        magicNumbers = new byte[MAGIC_NUMBER_LENGTH];
+        inputStream.read(magicNumbers, 0, MAGIC_NUMBER_LENGTH);
+    }
+
+    private void readMinorVersion() throws IOException
+    {
+        minorVersion = readU2();
+    }
+
+    private void readMajorVersion() throws IOException
+    {
+        majorVersion = readU2();
+    }
+
+    private void readConstantPoolSize() throws IOException
+    {
+        constantPoolSize = readU2();
+    }
+
+    private int readU2() throws IOException
+    {
+        byte [] byteArray = new byte[2];
+        inputStream.read(byteArray, 0, 2);
+
+        return (((int)byteArray[0]) << 8) | ((int)byteArray[1]);
+    }
+
+    public byte[] getMagicNumber()
+    {
+        return magicNumbers;
+    }
+
+    public int getMinorVersion()
+    {
+        return minorVersion;
+    }
+
+    public int getMajorVersion()
+    {
+        return majorVersion;
+    }
+
+    public Object getConstantPoolSize()
+    {
+        return constantPoolSize;
+    }
+}
---
+++ tests/JavaClassFileReaderTest.java
@@ -0,0 +1,45 @@
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class JavaClassFileReaderTest
+{
+    private JavaClassFileReader javaClassFileReader;
+
+    @Before
+    public void setUp() throws IOException
+    {
+        InputStream resourceAsStream = getClass().getResourceAsStream("Game_class");
+        javaClassFileReader = new JavaClassFileReader(resourceAsStream);
+    }
+
+    @Test
+    public void magicNumberIdentifiesAClassFile()
+    {
+        byte[] expected = {(byte)0xCA, (byte)0xFE, (byte)0xBA, (byte)0xBE};
+        assertArrayEquals(expected, javaClassFileReader.getMagicNumber());
+    }
+
+    @Test
+    public void minorVersionIs0x0000()
+    {
+        assertEquals(0, javaClassFileReader.getMinorVersion());
+    }
+
+    @Test
+    public void majorVersionIs0x0032()
+    {
+        assertEquals(0x32, javaClassFileReader.getMajorVersion());
+    }
+
+    @Test
+    public void constantPoolCountIs0x0043()
+    {
+        assertEquals(0x0043, javaClassFileReader.getConstantPoolSize());
+    }
+}
